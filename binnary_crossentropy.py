b081106fa942fdc8d8729b2c8c9941f13cf83a85
DoctorCotic <highfsociety@gmail.com>
2017-12-18 17:51:49 +0300

    fix

--- binnary_crossentropy.py
+++ binnary_crossentropy.py
@@ -1,23 +1,23 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
-import tensorflow as tf
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Embedding
 from keras.layers import LSTM, SpatialDropout1D
 from keras.datasets import imdb
 
-#Text analysis using recurrent neural networks
+# Text analysis using recurrent neural networks
 np.random.seed(42)
 # Maximum number of words in the dictionary
 max_features = 5000
 # Length of a review in words
 maxlen = 80
 
-#c = []
-#for d in ['/device:GPU:0', '/device:GPU:1']:
-  #  with tf.device(d):
+# c = []
+# for d in ['/device:GPU:0', '/device:GPU:1']:
+#  with tf.device(d):
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=max_features)
 X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
 X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
@@ -37,9 +37,9 @@ model.fit(X_train, y_train, batch_size=64, epochs=9, validation_data=(X_test, y_
 scores = model.evaluate(X_test, y_test,
                         batch_size=64)
 print("The accuracy of the model on test data : %.2f%%" % (scores[1] * 100))
-#with tf.device('/cpu:0'):
-  #  sum = tf.device(c)
+# with tf.device('/cpu:0'):
+#  sum = tf.device(c)
 ## Creates a session with log_device_placement set to True.
-#sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # Runs the op.
-#print(sess.run(sum))
+# print(sess.run(sum))
---
+++ iss533.py
@@ -0,0 +1,28 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input,decode_predictions
+import numpy as np
+import tensorflow as tf
+
+model = VGG16(weights='imagenet')
+# грузим предварительно обученные веса
+c = []
+for d in ['/device:GPU:0', '/device:GPU:1']:
+    with tf.device(d):
+        img = image.load_img('picture_test/test1.jpg', target_size=(224,224))
+        x = image.img_to_array(img)
+        #размерность
+        x = np.expand_dims(x, axis=0)
+        # предварительная обработка изображений
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+        print('Image Recognition Results',decode_predictions(preds,top = 5)[0])
+with tf.device('/cpu:0'):
+    sum = tf.device(c)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(sum))
